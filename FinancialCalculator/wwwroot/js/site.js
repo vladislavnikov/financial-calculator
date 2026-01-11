da1cbbd24d3ec9fab972470673b0eb382583898c
Vladislav <114019283+vladislavnikov@users.noreply.github.com>
2026-01-11 18:21:15 +0200

    Update site.js

--- FinancialCalculator/wwwroot/js/site.js
+++ FinancialCalculator/wwwroot/js/site.js
@@ -1,7 +1,4 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll("form.js-normalize-decimals").forEach(form => {
         const inputs = form.querySelectorAll(".decimal-input");
